8e3c3528c340ce436d4c36d3bc4cc3e25636f6fe
Bianca Oliveira <bianca@abngroup.com.au>
2020-05-05 10:34:45 +1000

    azure function app - mock up data

---
+++ HomesteadAzureFunctionApp/HomesteadAzureFunctionApp/HomesteadApi.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace HomesteadAzureFunctionApp
+{
+    public static class HomesteadApi
+    {
+        [FunctionName("GetSeasonData")]
+        public static async Task<IActionResult> RunGetAllSeasons(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/seasons")] HttpRequest req, ILogger log, ExecutionContext context)
+        {
+            log.LogInformation("HomesteadApi/GetSeasonData HTTP trigger function processed a request.");
+
+            // Using this setup allows Environment Variables to be set via local settings in development, and App Settings in Azure
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(context.FunctionAppDirectory)
+                            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                            .AddEnvironmentVariables()
+                            .Build();
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+            List<SeasonData> seasonDatas = MockDB.GetSeasonData();
+
+            return seasonDatas != null
+                ? (ActionResult)new OkObjectResult(seasonDatas)
+                : new BadRequestObjectResult("Error. No Season Data Found.");
+        }
+
+
+
+        [FunctionName("TemplateFunction")]
+        public static async Task<IActionResult> RunTemplate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
---
+++ HomesteadAzureFunctionApp/HomesteadAzureFunctionApp/MockDB.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+
+namespace HomesteadAzureFunctionApp
+{
+    public static class MockDB
+    {
+        public static List<SeasonData> GetSeasonData()
+        {
+            List<SeasonData> seasonDatas = new List<SeasonData>();
+            seasonDatas.Add(new SeasonData(1, "autumn", "tomatoes", "tomatoe.jpg", false, false, true));
+            seasonDatas.Add(new SeasonData(2, "autumn", "broccoli", "broc.jpg", true, true, false));
+            seasonDatas.Add(new SeasonData(3, "winter", "corn", "corn.jpg", true, true, false));
+
+            return seasonDatas;
+        }
+    }
+
+    public class SeasonData
+    {
+        public SeasonData(){ }
+        public SeasonData(int id, string season, string crop, string url, bool sow, bool seeding, bool harvest, bool complete = false)
+        {
+            Season = season;
+            Crop = crop;
+            IconUrl = url;
+            Sow = sow;
+            Seeding = seeding;
+            Harvest = harvest;
+            Complete = complete;
+            ID = id;
+        }
+
+       
+        public int ID { get; set; }
+        public string Season { get; set; }
+        public string Crop { get; set; }
+        public string IconUrl { get; set; }
+        public bool Sow { get; set; }
+        public bool Seeding { get; set; }
+        public bool Harvest { get; set; }
+        public bool Complete { get; set; }
+    }
+
+}
+
+
+/*
+ * public class GetNamesTest {
+    enum Colors { Red, Green, Blue, Yellow };
+    enum Styles { Plaid, Striped, Tartan, Corduroy };
+
+    public static void Main() {
+
+        Console.WriteLine("The members of the Colors enum are:");
+        foreach(string s in Enum.GetNames(typeof(Colors)))
+            Console.WriteLine(s);
+
+        Console.WriteLine();
+
+        Console.WriteLine("The members of the Styles enum are:");
+        foreach(string s in Enum.GetNames(typeof(Styles)))
+            Console.WriteLine(s);
+    }
+}
+// The example displays the following output:
+//       The members of the Colors enum are:
+//       Red
+//       Green
+//       Blue
+//       Yellow
+//       
+//       The members of the Styles enum are:
+//       Plaid
+//       Striped
+//       Tartan
+//       Corduroy
+
+    */
